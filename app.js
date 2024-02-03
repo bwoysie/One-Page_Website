6e26cbc5be1605eaf713549d7a438a82f0428251
Patrick Harrison <bwoysie876@gmail.com>
2024-02-03 10:54:07 -0500

    added files

---
+++ app.js
@@ -0,0 +1,37 @@
+function changeText1(){
+  document.getElementById('headerChange').innerHTML="LOrem Ipsum Dolor";
+  document.getElementById('paragraphChange').innerHTML= "The first inhabitants of Jamaica probably came from islands to the east in two waves of migration. About 600 CE the culture known as the “Redware people” arrived. Little is known of these people, however, beyond the red pottery they left behind.[1] Alligator Pond in Manchester Parish and Little River in St. Ann Parish are among the earliest known sites of this Ostionoid person, who lived near the coast and extensively hunted turtles and fish";
+
+}
+
+function changeText2(){
+  document.getElementById('headerChange').innerHTML=" Lorem Lorem Lorem";
+  document.getElementById('paragraphChange').innerHTML= "In 1655, the English invaded Jamaica, and defeated the Spanish. Some African enslaved people took advantage of the political turmoil and escaped to the island's interior mountains, forming independent communities which became known as the Maroons.[9] Meanwhile, on the coast, the English built the settlement of Port Royal, a base of operations where piracy flourished as so many European rebels had been rejected from their countries to serve sentences on the seas. Captain Henry Morgan, a Welsh plantation owner and privateer, raided settlements and shipping bases from Port Royal, earning him his reputation as one of the richest pirates in the Caribbean.";
+
+}
+
+function changeText3(){
+  document.getElementById('headerChange').innerHTML="Ipsum Ipsum Ipsum ";
+  document.getElementById('paragraphChange').innerHTML= "In the 18th century, sugar cane replaced piracy as British Jamaica's main source of income. The sugar industry was labour-intensive and the British brought hundreds of thousands of enslaved black Africans to the island. By 1850, the black and mulatto Jamaican population outnumbered the white population by a ratio of twenty to one. Enslaved Jamaicans mounted over a dozen major uprisings during the 18th century, including Tacky's Revolt in 1760. There were also periodic skirmishes between the British and the mountain communities of the Jamaican Maroons, culminating in the First Maroon War of the 1730s and the Second Maroon War of 1795 to 1796.";
+
+}
+
+function changeText4(){
+  document.getElementById('headerChange').innerHTML=" Dolor Dolor Dolor ";
+  document.getElementById('paragraphChange').innerHTML= "The aftermath of the Baptist War shone a light on the conditions of slaves which contributed greatly to the abolition movement and the passage of The Slavery Abolition Act of 1833, which formally ended slavery in Jamaica in 1834. However, relations between the white and black community remained tense coming into the mid-19th century, with the most notable event being the Morant Bay Rebellion in 1865. The latter half of the 19th century saw economic decline, low crop prices, droughts, and disease. When sugar lost its importance, many former plantations went bankrupt, and land was sold to Jamaican peasants and cane fields were consolidated by dominant British producers.";
+
+}
+
+function changeText5(){
+  document.getElementById('headerChange').innerHTML=" Magna Magna Magna";
+  document.getElementById('paragraphChange').innerHTML= "Jamaica's first political parties emerged in the late 1920s, while workers association and trade unions emerged in the 1930s. The development of a new Constitution in 1944, universal male suffrage, and limited self-government eventually led to Jamaican Independence in 1962 with Alexander Bustamante serving as its first prime minister. The country saw an extensive period of postwar growth and a smaller reliance on the agricultural sector and a larger reliance on bauxite and mining in the 1960s and 1970s. Political power changed hands between the two dominant parties, the JLP and PNP, from the 1970s to the present day. While Jamaica's murder rate fell by nearly half after the 2010 Tivoli Incursion, the country's murder rate remains one of the highest in the world. Economic troubles hit the country in 2013, the IMF agreed to a $1 billion loan to help Jamaica meet large debt payments, making Jamaica a highly indebted country that spends around half of its annual budget on debt repayments.";
+
+}
+
+function changeText6(){
+  document.getElementById('headerChange').innerHTML="Etiam Etiam Etiam";
+  document.getElementById('paragraphChange').innerHTML= "Tivoli Incursion gang of Tivoli Gardens from his father, Lester “Jim Brown” Coke, in the 1990s. Under Christopher Coke's leadership, the gang trafficked drugs and dabbled in visa fraud (using a high-school athletics team) and extortion, charging small traders in the nearby market for “protection money”. The gang had close political ties. Tivoli Gardens is part of the Kingston Western parliamentary district, a seat was held for years by Edward Seaga, long-time leader of the JLP. That helped Coke expand into construction, with his company winning numerous government contracts. Within Tivoli Gardens, the gang operated as a government unto itself.[142]"
+
+}
+
+
